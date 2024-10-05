3682f6ba4f3d5819aa01a8355e45c6e6fd5d7995
Andrew Ry <RyAndrew@users.noreply.github.com>
2024-10-05 17:16:25 -0500

    add date & time filtering, scrolling paging, favorites feature

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ const sharp = require('sharp')
 const { slowDown } = require('express-slow-down')
 const express = require('express')
 const hbs = require('hbs')
+const handlebars = new hbs.create()
+
+const Joi = require('joi')
 
 const imapAttachmentFetcher = require("./lib/imapAttachmentFetcher.js")
 const Utils = require("./lib/Utils.js")
@@ -22,6 +25,8 @@ const photoDir = path.join(__dirname,photoDirName)
 
 const app = express()
 const port = process.env.PORT || 8080
+
+const jsonBodyPayload = express.json()
 
 hbs.registerPartials(path.join(__dirname, 'views', 'partials'))
 app.set('view engine', 'hbs')
@@ -60,6 +65,7 @@ app.use('/'+photoDirName+thumbDir+'/:thumbname', async (req, res, next) => {
   })
 
 app.use('/static',express.static(path.join(__dirname,'static')))
+app.use('/favicon.ico',express.static(path.join(__dirname,'static','favicon.ico')))
 app.use('/'+photoDirName+thumbDir,express.static(photoDir+thumbDir))
 app.use('/'+photoDirName,express.static(photoDir))
 
@@ -74,7 +80,7 @@ app.get('/', async (req, res) => {
         lastEvent = 'Never'
     }else{
         const lastEventDate = new Date(lastEvent.maxDate)
-        lastEvent = lastEventDate.toLocaleString('en-US')
+        lastEvent = lastEventDate.toLocaleString('en-CA')
     }
 
     res.render('index', {lastEvent:lastEvent})
@@ -127,9 +133,116 @@ app.get('/fetch_all_emails', async (req, res) => {
     res.send(`Done!`)
 })
 
-app.get('/ai_events', async (req, res) => {
+app.get('/api/events', async (req, res) => {
 
-    const allEventsAttachments = await db.getAllEventsAndAttachments()
+    const dateStart = req.query?.dateStart
+    if(!dateStart){
+        res.send( 'missing dateStart parameter' )
+        return
+    }
+
+    const dateEnd = req.query?.dateEnd
+    if(!dateEnd){
+        res.send( 'missing dateEnd parameter' )
+        return
+    }
+
+    const after = req.query?.after
+    if(!after){
+        res.send( 'missing after parameter' )
+        return
+    }
+    
+    // const dateStart = req.query?.dateStart
+    // if(!dateStart){
+    //     res.send( 'missing dateStart parameter' )
+    //     return
+    // }
+    const allEventsAttachments = await db.queryEventsAndAttachmentsOffset(after, dateStart, dateEnd)
+    console.log('allEventsAttachments.length',allEventsAttachments.length)
+    
+    let eventsById = {}
+    //let lastEvent
+
+    allEventsAttachments.forEach(event => {
+        //console.log(event)
+        if(!eventsById[event.eventid]){
+            eventsById[event.eventid]={
+                eventid: event.eventid,
+                favorite: event.favorite,
+                type: Utils.formatEventType(event.type),
+                camera: Utils.formatCamera(event.camera),
+                datetime: event.datetime,
+                niceDate: Utils.formatDateYmd(event.datetime),
+                niceTime: Utils.formatDateHms(event.datetime),
+                text: event.text,
+                attachments: [],
+            }
+            //lastEvent = event.eventid
+        }
+        eventsById[event.eventid].attachments.push({
+            filename: event.filename,
+        })
+    })
+    let events = Object.values(eventsById)
+
+    events = Utils.sortObjectArray(events, {
+        key:'datetime',
+        direction:'desc'
+    })
+
+    let highQuality = (req.query?.quality === 'low' ? false : true)
+    //const carousel = 
+    //console.log('carousel',carousel)
+    const template = handlebars.compile(await fs.readFile("views/partials/event_carousel.hbs","utf8"))
+    
+    //console.log()
+
+    res.send( JSON.stringify({
+        count:allEventsAttachments.length,
+        events,
+        html:template({events, highQuality})
+    }))
+
+    // res.render("partials/event_carousel.hbs",(err, renderedHtml) => {
+    //     if(err) {
+    //         console.log('error',err)
+    //         return
+    //     }
+    //     console.log(arguments)
+    //     console.log(renderedHtml)
+    // })
+})
+
+const putFavoriteSchema = Joi.object({
+    eventId: Joi.number().integer().positive(),
+    favorite: Joi.number().integer().min(0).max(1),
+})
+
+app.put('/favorite', jsonBodyPayload, Utils.joiValidatorMiddleware(putFavoriteSchema), async (req, res) => {
+
+    console.log('req.body',req.body)
+    console.log('req.body.eventId',req.body.eventId)
+    console.log('req.body.favorite',req.body.favorite)
+
+
+    //const { error, value } = putFavoriteSchema.validate(req.body);
+
+    // if(error){
+    //     res.status(400).send('invalid input')
+    //     console.log('invalid input')
+    //     return
+    // }
+
+    await db.updateEventFavorite(req.body.eventId, req.body.favorite)
+    
+    res.send()
+})
+app.get('/events', async (req, res) => {
+
+    const after = req.query?.after || null
+
+    const allEventsAttachments = await db.queryEventsAndAttachmentsOffset(after)
     
     let eventsById = {}
     let lastEvent
@@ -159,10 +272,36 @@ app.get('/ai_events', async (req, res) => {
         direction:'desc'
     })
 
-    let highQuality = req.query?.quality === 'high' ? true : false;
+    let highQuality = (req.query?.quality === 'low' ? false : true)
+
+    //console.log(req.query)
+    //console.log('highQuality',highQuality)
+
+    let dateStart = req.query?.dateStart
+    if(!dateStart){
+        dateStart = new Date(Date.now() - 86400000)
+    }
+    let dateEnd = req.query?.dateEnd
+    if(!dateEnd){
+        dateEnd = new Date()
+    }
+    //console.log('datestart',datestart,'dateend',dateend)
     
-    res.render('ai_events',{highQuality:highQuality, events:events})
+    res.render('events',{highQuality, events, dateStart, dateEnd})
 })
+
+
+function errorHandler (err, req, res, next) {
+    if(err){
+        console.log('error Handler!')
+        console.log(err)
+        res.status(500)
+        res.send('Server Error')
+        return
+    }
+    next()
+}
+app.use(errorHandler)
 
 process.on('unhandledRejection', (reason) => {
     console.log('unhandledRejection')
